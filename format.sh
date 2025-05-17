c3b6808f2ab9f204e015a486b8560b5afd896a30
bhuban <bhuban@nsdai.me>
2025-05-17 09:53:37 +0545

    Fromat add

---
+++ format.sh
@@ -0,0 +1,17 @@
+rm -r system/migrations
+rm -r accounts/migrations
+rm -r room/migrations
+rm -r events/migrations
+rm -r grocery/migrations
+rm -r book/migrations
+rm -r payment/migrations
+
+
+rm db.sqlite3
+
+pip install -r requirements.txt
+
+python manage.py makemigrations system accounts room events grocery book payment
+python manage.py migrate
+#python manage.py loaddata system/initial_fixtures/initial_fixtures.json
+#python manage.py loaddata accounts/initial_fixtures/initial_fixtures.json
