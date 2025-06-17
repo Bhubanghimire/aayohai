de04877fd1b9b90cd8727992ff3f297caf60860f
bhuban <bhuban@nsdai.me>
2025-06-17 19:50:21 +0545

    grocery list update

--- grocery/normal_user/urls.py
+++ grocery/normal_user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from accounts.views import AuthViewSet
-from grocery.normal_user.views import GroceryDashboardViewSet
+from grocery.normal_user.views import GroceryDashboardViewSet, RoomSearchViewSet
 
 grocery_router = DefaultRouter()
 grocery_router.register(r'dashboard', GroceryDashboardViewSet, basename="grocery_dashboard")
+grocery_router.register(r'search-list', RoomSearchViewSet, basename="grocery_search")
