85af4eca39b7a3020f48bc345cc330ffa0de74a5
Bhubanghimire <bhubanghimire3130@gmail.com>
2025-07-03 17:00:42 +0545

    update admin

--- grocery/admin.py
+++ grocery/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from grocery.models import Grocery
+from grocery.models import Grocery, GroceryCategory, Gallery
 
 
 # Register your models here.
 @admin.register(Grocery)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id',"quantity_unit",'created_at','updated_at')
+
+@admin.register(GroceryCategory)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id',)
+
+@admin.register(Gallery)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id',)
+
